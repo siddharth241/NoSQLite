7d8431c68257243f3e8bedfe60a5f4e2691afb87
Jimish_Shah <shah1bu@uwindsor.ca>
2021-07-22 08:09:20 +0000

    MongoDB insert/delete/find suport added

--- src/mongoDB/mongo.cpp
+++ src/mongoDB/mongo.cpp
@@ -1,8 +1,22 @@
 #include "mongo.h"
 #include "enums.h"
+#include <cstdarg>
+#include <utility>
 
 namespace NoSQLite
 {
+  using bsoncxx::builder::basic::kvp;
+  using bsoncxx::builder::basic::make_document;
+  using bsoncxx::builder::basic::make_array;
+  using bsoncxx::builder::stream::document;
+  using bsoncxx::builder::stream::finalize;
+
+  void Mongo::connect()
+  {
+    mongocxx::instance instance{};
+    mongocxx::client conn{mongocxx::uri{DEF_URI + ":" + std::to_string(DEF_PORT)}};
+  }
+
   int Mongo::insert(int operationType, ...)
   {
     std::cout << "Mongo::Insert operationType: " << operationType << std::endl;
@@ -10,6 +24,25 @@ namespace NoSQLite
     {
       case MongoOpType::INSERT_ONE:
         {
+          std::string key,value,collection;
+          std::va_list args;
+
+          va_start(args,operationType);
+          key = va_arg(args,std::string);
+          value = va_arg(args,std::string);
+          collection = va_arg(args,std::string);
+          va_end(args);
+
+          mongocxx::client conn{mongocxx::uri{DEF_URI + ":" + std::to_string(DEF_PORT)}};	
+          auto db = conn[Mongo::DB_NAME];
+          std::cout << "Creating Doc: " << std::endl;
+
+          bsoncxx::document::value coll_doc = make_document(kvp(key,value));
+          auto res = db[collection].insert_one(std::move(coll_doc));
+
+          std::cout << "Created Doc: " << std::endl;
+
+          // std::cout << "Response: " << res << std::endl;	   
         }
         break;
 
@@ -29,6 +62,18 @@ namespace NoSQLite
     {
       case MongoOpType::DELETE_ONE:
         {
+          std::string key,value,collection;
+          std::va_list args;
+
+          va_start(args,operationType);
+          key = va_arg(args,std::string);
+          value = va_arg(args,std::string);
+          collection = va_arg(args,std::string);
+          va_end(args);
+
+          mongocxx::client conn{mongocxx::uri{DEF_URI + ":" + std::to_string(DEF_PORT)}};
+          auto db = conn[Mongo::DB_NAME];
+          db[collection].delete_one(make_document(kvp(key,value)));
         }
         break;
 
@@ -40,7 +85,7 @@ namespace NoSQLite
 
     return (int)MongoError::SUCCESS;
   }
-  
+
   int Mongo::find(int operationType, ...)
   {
     std::cout << "Mongo::find operationType: " << operationType << std::endl;
@@ -48,6 +93,22 @@ namespace NoSQLite
     {
       case MongoOpType::FIND_ONE:
         {
+          std::string key,value,collection;
+          std::va_list args;
+
+          va_start(args,operationType);
+          key = va_arg(args,std::string);
+          value = va_arg(args,std::string);
+          collection = va_arg(args,std::string);
+          va_end(args);
+
+          mongocxx::client conn{mongocxx::uri{DEF_URI + ":" + std::to_string(DEF_PORT)}};
+          auto db = conn[Mongo::DB_NAME];
+          auto cursor = db[collection].find(make_document(kvp(key,value)));
+          for(auto&& doc : cursor){
+            std::cout << bsoncxx::to_json(doc) << std::endl;
+          }
+
         }
         break;
 
--- src/mongoDB/mongo.h
+++ src/mongoDB/mongo.h
@@ -2,6 +2,29 @@
 #define NOSQLITE_MONGO_H
 
 #include "../noSQL.h"
+//#include "enums.h"
+#include <cstdlib>
+
+
+#include <chrono>
+#include <bsoncxx/builder/basic/array.hpp>
+#include <bsoncxx/builder/basic/document.hpp>
+#include <bsoncxx/types.hpp>
+#include <mongocxx/client.hpp>
+#include <mongocxx/instance.hpp>
+#include <mongocxx/uri.hpp>
+#include <bsoncxx/builder/stream/document.hpp>
+#include <bsoncxx/json.hpp>
+
+#include <memory>
+#include <bsoncxx/stdx/make_unique.hpp>
+#include <bsoncxx/stdx/optional.hpp>
+#include <bsoncxx/stdx/string_view.hpp>
+#include <mongocxx/instance.hpp>
+#include <mongocxx/logger.hpp>
+#include <mongocxx/pool.hpp>
+#include <mongocxx/uri.hpp>
+
 
 namespace NoSQLite
 {
@@ -10,11 +33,20 @@ namespace NoSQLite
     public:
       using NoSQL::insert;  // Pull in the Split symbol from the parent class
 
+      const std::string DB_NAME = "APIDB";
+      const int DEF_PORT = 27017;
+      const std::string DEF_URI = "mongodb://localhost";
+
+
       virtual int insert(int operationType, ...) override;
-      
+
       virtual int remove(int operationType, ...) override;
 
       virtual int find(int operationType, ...) override;
+
+      void connect();
+
+
   };
 }
 #endif
